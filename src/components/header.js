88f64f9432c8de1a48580f88fcdcb65843980326
KFamurewa <kayla.famurewa@gmail.com>
2022-04-16 02:37:13 -0400

    wip-landing page setup

---
+++ src/components/header.js
@@ -0,0 +1,19 @@
+
+
+export default function Header() {
+
+    return (
+      <div>
+        <header className="headerContainer">
+            <a className='logo' href='#'>Kayla Famurewa</a>
+            <div className='nav'>
+            <a href='#'>work</a>
+            <a href='#'>about</a>
+            <a href='#'>services</a>
+            <a href='#'>contact</a>
+            </div>
+          </header>
+        </div>
+    )
+  
+  }
